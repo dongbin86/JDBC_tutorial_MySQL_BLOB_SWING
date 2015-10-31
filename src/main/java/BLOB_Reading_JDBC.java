15b69306c16b6da0bd4386b4ead19347bb376a2e
ronaldo173 <my@mail>
2015-10-31 23:28:40 +0200

    Changed reading -->to read all BLOBs from DB and saving them into new directory!

--- src/main/java/BLOB_Reading_JDBC.java
+++ src/main/java/BLOB_Reading_JDBC.java
@@ -1,8 +1,6 @@
 import java.io.*;
-import java.sql.Connection;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
+import java.util.ArrayList;
 
 /**
  * Created by Santer on 31.10.2015.
@@ -16,11 +14,15 @@ public class BLOB_Reading_JDBC {
         InputStream inputStream = null;
         FileOutputStream fileOutputStream = null;
 
+        int filesCount = 0;
+
         try{
             connection=JDBC_Connection.getJdbc_connection().getConnection();
             statement=connection.createStatement();
-            String sql = "SELECT resume FROM employees where id=2";
+            int i = 1;
+            String sql = "SELECT resume FROM employees where id=" + i;
             resultSet=statement.executeQuery(sql);
+
 
             File gettedFile = new File("geted_from_DB.jpg");
             fileOutputStream = new FileOutputStream(gettedFile);
@@ -37,6 +39,52 @@ public class BLOB_Reading_JDBC {
 
                 System.out.println("\nSaved to file : " + gettedFile.getAbsolutePath());
                 System.out.println("SUCCESS!");
+
+                resultSet=statement.executeQuery("SELECT COUNT(resume) FROM employees");
+                while (resultSet.next()){
+                    filesCount = resultSet.getInt(1);
+                }
+                System.out.println("\n\nFiles  BLOB = " +filesCount);
+                ArrayList<String > names = new ArrayList<String>();
+                for (int j = 0; j < filesCount; j++) {
+                    names.add("File_" + j + ".jpg");
+                }
+                System.out.println(names);
+
+                //get id of BLOB
+                ArrayList<Integer> idOfBlobs = new ArrayList<Integer>();
+                resultSet = statement.executeQuery("SELECT id FROM employees where resume IS NOT NULL ");
+                while (resultSet.next()){
+                    idOfBlobs.add(resultSet.getInt(1));
+                }
+                System.out.println("Id where BLOB in 'resume' not null: " + idOfBlobs);
+
+                //go to read ALL files BLOB from column
+                //read from BLOB and write into file
+//read BLOB
+                    for (int k = 0; k < idOfBlobs.size(); k++) {
+                        //make files
+                        File file1 = new File("C:\\Users\\Santer\\Desktop\\Development\\Projects\\JDBC_tutorial_MySQL_BLOB_SWING\\files");
+                        if (!file1.exists()) {
+                            file1.mkdir();
+                        }
+                        File file = new File(file1 + "\\" + names.get(k));
+                        fileOutputStream = new FileOutputStream(file);
+
+                        resultSet = statement.executeQuery("SELECT resume FROM employees WHERE id =" + idOfBlobs.get(k));
+                        if (resultSet.next()) {
+                            Blob blob = resultSet.getBlob("resume");
+                            inputStream = blob.getBinaryStream();
+                            fileOutputStream = new FileOutputStream(file);
+
+                            byte[] buffer1 = new byte[4096];
+                            int bytesRead = -1;
+                            while ((bytesRead = inputStream.read(buffer1)) != -1) {
+                                fileOutputStream.write(buffer1, 0, bytesRead);
+                            }
+                        }
+
+                    }
             }
 
         } catch (SQLException e) {
