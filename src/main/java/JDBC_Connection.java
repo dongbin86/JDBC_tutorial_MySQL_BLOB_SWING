1b2250bdfd1ff27ba22d39ec826ea7713fd59077
ronaldo173 <my@mail>
2015-11-01 14:57:25 +0200

    Added reading if sitings with connecting to DB from file.promerties

--- src/main/java/JDBC_Connection.java
+++ src/main/java/JDBC_Connection.java
@@ -1,6 +1,10 @@
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.Properties;
 
 /**
  * Created by Santer on 31.10.2015.
@@ -8,15 +12,28 @@ import java.sql.SQLException;
 public class JDBC_Connection {
     private static Connection connection = null;
     private static JDBC_Connection jdbc_connection = null;
-    private final String URL = "jdbc:mysql://localhost:3306/demo";
-    private final String USERNAME = "root";
-    private final String PASSWORD = "root";
+    Properties properties = new Properties();
+
+    private String URL = null;
+    private  String USERNAME = null;
+    private  String PASSWORD = null;
 
     private JDBC_Connection(){}
     public Connection getConnection(){
+
         try {
+            //get from properites sittings
+            properties.load(new FileInputStream("C:\\Users\\Santer\\Desktop\\Development\\Projects\\JDBC_tutorial_MySQL_BLOB_SWING\\src\\main\\java\\prop.properties"));
+            URL = properties.getProperty("dbUrl");
+            USERNAME = properties.getProperty("user");
+            PASSWORD=properties.getProperty("password");
+            System.out.println(URL + " " + USERNAME + " " + PASSWORD);
             connection= DriverManager.getConnection(URL, USERNAME, PASSWORD);
         } catch (SQLException e) {
+            e.printStackTrace();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
             e.printStackTrace();
         }
         return connection;
